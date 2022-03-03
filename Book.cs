13cf928aa896f8da6ab1438ea611cc64f489acfe
sorachung <sorachung.src@gmail.com>
2022-03-03 09:19:32 -0600

    created Book class

---
+++ Book.cs
@@ -0,0 +1,10 @@
+namespace Library
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string PublishDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
